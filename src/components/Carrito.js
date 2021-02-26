75f2d001330623fefd4d10f749e13333a29d5808
hectoralarcon <hectoralarconguerra@gmail.com>
2021-02-25 23:18:09 -0500

    MOstrando los elementos del carrito

---
+++ src/components/Carrito.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './carrito.css'
+
+const Carrito = () => (
+    <div className="carrito">
+        <h2>Tu carrito de compritas</h2>
+    </div>
+);
+
+ 
+export default Carrito;
